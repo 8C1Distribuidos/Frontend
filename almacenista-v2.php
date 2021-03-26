f80fe25aa0f6a335854dce69267ae711f788d017
QuiquePCB <quiquepcb@gmail.com>
2021-03-26 13:28:42 -0600

    Correcciones almacenista, select categorías

--- almacenista-v2.php
+++ almacenista-v2.php
@@ -55,10 +55,11 @@
 				</div>
 				<div class="modal-body">
 					<label>Ingrese el nombre</label>
-					<input type="text" name="nombre" id="nombre" class="form-control required" />
+					<input type="text" name="nombre" id="name" class="form-control required" />
 					<br />
 					<label>Ingrese clasificacion</label>
-					<input type="text" name="clasificacion" id="clasificacion" class="form-control required" />
+                    <select class="form-control" id="clasificacion_menu"></select>
+					<!--<input type="text" name="clasificacion" id="clasificacion" class="form-control required" />-->
                     <br/>
                     <label>Catalogo</label>
 					<input type="text" name="catalogo" id="catalogo" class="form-control required" disabled/>
@@ -97,11 +98,21 @@
                         "<td>"+obj[i]["clasificacion"][0]["name"]+"</td>"+
                         "<td>"+obj[i]["clasificacion"][0]["catalogo"][0]["name"]+"</td>"+
                         "<td>"+obj[i]["stock"]+"</td>"+
-                        "<td>"+"$"+obj[i]["precio"]+"</td>"+
+                        "<td>"+"$"+obj[i]["costo"]+"</td>"+
                         "<td><button type='button' name='update' id="+obj[i]["id"]+ " class='btn btn-default update-circle-button update' ><svg xmlns=http://www.w3.org/2000/svg width=16 height=16 fill=currentColor class='bi bi-pencil-fill' viewBox=0 0 16 16><path d='M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z'/></svg></button></td>"+
                         "<td><button type=button name=delete id="+obj[i]["id"]+ " class= 'btn btn-default delete-circle-button delete'><svg xmlns=http://www.w3.org/2000/svg width=16 height=16 fill=currentColor class='bi bi-trash-fill' viewBox=0 0 16 16><path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z'/></svg></button></td></tr>";
             $("#productos_data").append(tr);
             }
+        });
+        $.getJSON("http://localhost:3000/clasificacion", function( data ) {
+            var obj = data;
+            for(var i=0;i<obj.length;i++)
+            {
+                var option  ="<option>"+obj[i]["name"]+"</option>";
+                $("#clasificacion_menu").append(option);
+                $("#catalogo").append(obj[i]["catalogo"][0]["name"]);
+            }
+            
         });
         $(document).on('click', '.update', function(){
             var productos_id = $(this).attr("id");
@@ -110,11 +121,11 @@
                 var obj = data;
                 $('#productosModal').modal('show');
                 $('.modal-title').text("Editar producto");
-                $('#nombre').val(obj[0]["name"]);
+                $('#name').val(obj[0]["name"]);
                 $('#clasificacion').val(obj[0]["clasificacion"][0]["name"]);
                 $('#catalogo').val(obj[0]["clasificacion"][0]["catalogo"][0]["name"]);
                 $('#stock').val(obj[0]["stock"]);
-                $('#costo').val(obj[0]["precio"]);
+                $('#costo').val(obj[0]["costo"]);
                 $('#producto_id').val(productos_id);
                 $('#productos_uploaded_image').html(obj[0]["imagen"]);
                 $('#action').val("Update");
@@ -171,7 +182,7 @@
                     }
                 });
             }else{
-                //PUT actualizar 
+                //UPDATE
             }     
 	    });
         $(document).on('click', '.delete', function(){
