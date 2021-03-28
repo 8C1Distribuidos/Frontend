6aa5d4565fd1d886984e27eab6a69f86f175a0f7
Ferny26 <61126222+Ferny26@users.noreply.github.com>
2021-03-27 23:53:56 -0600

    loadTable

--- almacenista-v2.php
+++ almacenista-v2.php
@@ -39,6 +39,9 @@
 							<th width="10%">Borrar</th>
 						</tr>
 					</thead>
+                    <tbody>
+
+                    </tbody>
 				</table>
 			</div>
 		
@@ -87,23 +90,29 @@
 
 <script type="text/javascript" language="javascript">
     $(document).ready(function(){
+        var obj;
         $.getJSON("http://localhost:3000/products", function( data ) {
-            var obj = data;
+            obj = data;
+            loadTable();
+        });
+        function loadTable(){
             for(var i=0;i<obj.length;i++)
             {
-                var tr  ="<tr>"+
-                        "<td><img src="+obj[i]["imagen"]+"></td>"+
-                        "<td>"+obj[i]["id"]+"</td>"+
-                        "<td>"+obj[i]["name"]+"</td>"+
-                        "<td>"+obj[i]["clasificacion"][0]["name"]+"</td>"+
-                        "<td>"+obj[i]["clasificacion"][0]["catalogo"][0]["name"]+"</td>"+
-                        "<td>"+obj[i]["stock"]+"</td>"+
-                        "<td>"+"$"+obj[i]["costo"]+"</td>"+
-                        "<td><button type='button' name='update' id="+obj[i]["id"]+ " class='btn btn-default update-circle-button update' ><svg xmlns=http://www.w3.org/2000/svg width=16 height=16 fill=currentColor class='bi bi-pencil-fill' viewBox=0 0 16 16><path d='M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z'/></svg></button></td>"+
-                        "<td><button type=button name=delete id="+obj[i]["id"]+ " class= 'btn btn-default delete-circle-button delete'><svg xmlns=http://www.w3.org/2000/svg width=16 height=16 fill=currentColor class='bi bi-trash-fill' viewBox=0 0 16 16><path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z'/></svg></button></td></tr>";
+            var tr  ="<tr>"+
+                    "<td><img src="+obj[i]["imagen"]+"></td>"+
+                    "<td>"+obj[i]["id"]+"</td>"+
+                    "<td>"+obj[i]["name"]+"</td>"+
+                    "<td>"+obj[i]["clasificacion"][0]["name"]+"</td>"+
+                    "<td>"+obj[i]["clasificacion"][0]["catalogo"][0]["name"]+"</td>"+
+                    "<td>"+obj[i]["stock"]+"</td>"+
+                    "<td>"+"$"+obj[i]["costo"]+"</td>"+
+                    "<td><button type='button' name='update' id="+obj[i]["id"]+ " class='btn btn-default update-circle-button update' ><svg xmlns=http://www.w3.org/2000/svg width=16 height=16 fill=currentColor class='bi bi-pencil-fill' viewBox=0 0 16 16><path d='M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z'/></svg></button></td>"+
+                    "<td><button type=button name=delete id="+obj[i]["id"]+ " class= 'btn btn-default delete-circle-button delete'><svg xmlns=http://www.w3.org/2000/svg width=16 height=16 fill=currentColor class='bi bi-trash-fill' viewBox=0 0 16 16><path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z'/></svg></button></td></tr>";
             $("#productos_data").append(tr);
-            }
-        });
+            };
+        }
+        
+        
         $.getJSON("http://localhost:3000/clasificacion", function( data ) {
             var obj = data;
             for(var i=0;i<obj.length;i++)
@@ -167,7 +176,6 @@
             event.preventDefault();
             var x= $('#action').val();
             if( x == "Add"){
-
                 $.ajax({
                     url:"http://localhost:3000/products",
                     type:"POST",
@@ -178,7 +186,9 @@
                     {
                         $('#productos_form')[0].reset();
                         $('#productosModal').modal('hide');
-                        //dataTable.ajax.reload();
+                        obj.push(json);
+                        $('#productos_data > tbody').empty();
+                        loadTable();
                     }
                 });
             }else{
@@ -196,8 +206,14 @@
                     contentType:"application/json",
                     success:function(data)
                     {
-                        alert(data);
-                        dataTable.ajax.reload();
+                        for( var i = 0; i < obj.length; i++){     
+                            if ( obj[i]["id"] == productos_id) { 
+                                obj.splice(i, 1); 
+                                break; 
+                            }
+                        }  
+                        $('#productos_data > tbody').empty();
+                        loadTable();
                     }
                 });
             }
