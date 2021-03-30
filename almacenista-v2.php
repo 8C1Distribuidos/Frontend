8f182e062f7e3ed584c166b6b3c101cf5d231edf
Ferny26 <61126222+Ferny26@users.noreply.github.com>
2021-03-30 12:49:42 -0600

    acomodoAlmacenista

--- almacenista-v2.php
+++ almacenista-v2.php
@@ -58,14 +58,13 @@
 				</div>
 				<div class="modal-body">
 					<label>Ingrese el nombre</label>
-					<input type="text" name="nombre" id="name" class="form-control required" />
+					<input type="text" name="name" id="name" class="form-control required" />
 					<br />
 					<label>Ingrese clasificacion</label>
-                    <select class="form-control" id="clasificacion_menu"></select>
-					<!--<input type="text" name="clasificacion" id="clasificacion" class="form-control required" />-->
+                    <select class="form-control" name="clasificacion" id="clasificacion_menu"></select>
                     <br/>
                     <label>Catalogo</label>
-					<input type="text" name="catalogo" id="catalogo" class="form-control required" disabled/>
+					<input type="text" name="catalogo" id="catalogo" class="form-control"/>
 					<br />
                     <label>Ingrese stock</label>
 					<input type="number" name="stock" id="stock" class="form-control required" />
@@ -90,56 +89,69 @@
 
 <script type="text/javascript" language="javascript">
     $(document).ready(function(){
-        var obj;
+        //Varibles
+        var products;
+        var classifications;
+
+        //GET productos
         $.getJSON("http://localhost:3000/products", function( data ) {
-            obj = data;
+            products = data;
             loadTable();
         });
+
+        //Formato de productos en tabla 
         function loadTable(){
-            for(var i=0;i<obj.length;i++)
+            for(var i=0;i<products.length;i++)
             {
             var tr  ="<tr>"+
-                    "<td><img src="+obj[i]["imagen"]+"></td>"+
-                    "<td>"+obj[i]["id"]+"</td>"+
-                    "<td>"+obj[i]["name"]+"</td>"+
-                    "<td>"+obj[i]["clasificacion"][0]["name"]+"</td>"+
-                    "<td>"+obj[i]["clasificacion"][0]["catalogo"][0]["name"]+"</td>"+
-                    "<td>"+obj[i]["stock"]+"</td>"+
-                    "<td>"+"$"+obj[i]["costo"]+"</td>"+
-                    "<td><button type='button' name='update' id="+obj[i]["id"]+ " class='btn btn-default update-circle-button update' ><svg xmlns=http://www.w3.org/2000/svg width=16 height=16 fill=currentColor class='bi bi-pencil-fill' viewBox=0 0 16 16><path d='M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z'/></svg></button></td>"+
-                    "<td><button type=button name=delete id="+obj[i]["id"]+ " class= 'btn btn-default delete-circle-button delete'><svg xmlns=http://www.w3.org/2000/svg width=16 height=16 fill=currentColor class='bi bi-trash-fill' viewBox=0 0 16 16><path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z'/></svg></button></td></tr>";
+                    "<td><img src="+products[i]["imagen"]+"></td>"+
+                    "<td>"+products[i]["id"]+"</td>"+
+                    "<td>"+products[i]["name"]+"</td>"+
+                    "<td>"+products[i]["clasificacion"][0]["name"]+"</td>"+
+                    "<td>"+products[i]["clasificacion"][0]["catalogo"][0]["name"]+"</td>"+
+                    "<td>"+products[i]["stock"]+"</td>"+
+                    "<td>"+"$"+products[i]["costo"]+"</td>"+
+                    "<td><button type='button' name='update' id="+products[i]["id"]+ " class='btn btn-default update-circle-button update' ><svg xmlns=http://www.w3.org/2000/svg width=16 height=16 fill=currentColor class='bi bi-pencil-fill' viewBox=0 0 16 16><path d='M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z'/></svg></button></td>"+
+                    "<td><button type=button name=delete id="+products[i]["id"]+ " class= 'btn btn-default delete-circle-button delete'><svg xmlns=http://www.w3.org/2000/svg width=16 height=16 fill=currentColor class='bi bi-trash-fill' viewBox=0 0 16 16><path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z'/></svg></button></td></tr>";
             $("#productos_data").append(tr);
             };
         }
         
-        
+        //GET clasificaciones 
         $.getJSON("http://localhost:3000/clasificacion", function( data ) {
-            var obj = data;
-            for(var i=0;i<obj.length;i++)
+            classifications = data;
+            for(var i=0;i<classifications.length;i++)
             {
-                var option  ="<option>"+obj[i]["name"]+"</option>";
+                var option  ="<option value="+classifications[i]["id"]+">"+classifications[i]["name"]+"</option>";
                 $("#clasificacion_menu").append(option);
-                $("#catalogo").append(obj[i]["catalogo"][0]["name"]);
             }
+            document.getElementById("catalogo").value = classifications[0]["catalogo"][0]["name"];
             
         });
+
+        //Select On change 
+        $('#clasificacion_menu').change(function(){
+            var obj = classifications.find( clasification => clasification.id ==  this.value);
+            document.getElementById("catalogo").value = obj.catalogo[0].name;
+        })
+
+        //Modal UPDATE
         $(document).on('click', '.update', function(){
-            var productos_id = $(this).attr("id");
-            var url  = "http://localhost:3000/products?id="+productos_id;
-            $.getJSON(url, function( data ) {
-                var obj = data;
-                $('#productosModal').modal('show');
+            const obj = products.find( product => product.id ==  $(this).attr("id"));
+            $('#productosModal').modal('show');
                 $('.modal-title').text("Editar producto");
-                $('#name').val(obj[0]["name"]);
-                $('#clasificacion').val(obj[0]["clasificacion"][0]["name"]);
-                $('#catalogo').val(obj[0]["clasificacion"][0]["catalogo"][0]["name"]);
-                $('#stock').val(obj[0]["stock"]);
-                $('#costo').val(obj[0]["costo"]);
-                $('#producto_id').val(productos_id);
-                $('#productos_uploaded_image').html(obj[0]["imagen"]);
+                $('#name').val(obj.name);
+                $('#clasificacion').val(obj.clasificacion.name);
+                $('#catalogo').val(obj.clasificacion[0].catalogo[0].name);
+                $('#stock').val(obj.stock);
+                $('#costo').val(obj.costo);
+                $('#id').val(obj.id);
+                $('#productos_uploaded_image').html(obj.imagen);
                 $('#action').val("Update");
-            });
         });
+
+
+        //ADD Boton
         $('#add_button').click(function(){
             $('#productos_form')[0].reset();
             $('.modal-title').text("Añadir Producto");
@@ -147,17 +159,21 @@
             $('#productos_uploaded_image').html('');
         });
 
+        //POST/UPDATE producto
         $(document).on('submit', '#productos_form',function(event){
+            event.preventDefault();
             var extension = $('#imagen').val().split('.').pop().toLowerCase();
             if(extension != '')
             {
                 if(jQuery.inArray(extension, ['gif','png','jpg','jpeg']) == -1)
                 {
-                    alert("Invalid Image File");
+                    alert("Formato de imagen no valido");
                     $('#imagen').val('');
                     return false;
                 }
             }
+
+            //Formulario a JSON
             function toJSONString( form ) {
                 var obj = {};
                 var elements = form.querySelectorAll( "input, select, textarea" );
@@ -165,17 +181,30 @@
                     var element = elements[i];
                     var name = element.name;
                     var value = element.value;
-
-                    if( name && name!="action") {
+                    if( name && name=="clasificacion") {
+                        var clasificacion = classifications.find( clasification => clasification.id ==  value);
+                        obj[name] = {
+                            id: clasificacion.id,
+                            name: clasificacion.name
+                        };
+                        //alert(clasificacion.name);
+                        //obj[name] = clasificacion;
+                        //obj[name][0]["id"] = clasificacion.id;
+                        //obj[name][0]["name"] = clasificacion.name;
+                        //obj[name]["catalogo"][0]["id"] = clasificacion.catalogo[0].id;
+                        //obj[name]["catalogo"][0]["name"] = clasificacion.catalogo[0].name;*/
+                    }
+                    if( name && name!="action" && name!="catalogo") {
                         obj[ name ] = value;
                     }
                 }
                 return JSON.stringify( obj );
             }
+            
             var json = toJSONString( this );
-            event.preventDefault();
-            var x= $('#action').val();
-            if( x == "Add"){
+
+            if( $('#action').val() == "Add"){
+                //POST
                 $.ajax({
                     url:"http://localhost:3000/products",
                     type:"POST",
@@ -184,6 +213,7 @@
                     contentType:"application/json",
                     success:function(data)
                     {
+                        alert(data["name"]);
                         $('#productos_form')[0].reset();
                         $('#productosModal').modal('hide');
                         obj.push(json);
